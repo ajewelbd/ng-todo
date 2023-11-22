da915dd1e9a7a55dfd4d1d03315ff07b5b4c10c5
Md. Ashraful Alam <imobilzbd@gmail.com>
2023-11-22 19:10:13 +0600

    feat: todo add button added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { TodosComponent } from './todos/todos.component';
+import { NewTodoComponent } from './new-todo/new-todo.component';
+import { TodoComponent } from './todo/todo.component';
 
 export const routes: Routes = [
     {
         path: "",
         component: TodosComponent
+    },
+    {
+        path: "new-todo",
+        component: NewTodoComponent
+    },
+    {
+        path: "todo/:id",
+        component: TodoComponent
     }
 ];
---
+++ src/app/new-todo/new-todo.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-new-todo',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './new-todo.component.html',
+  styleUrl: './new-todo.component.scss'
+})
+export class NewTodoComponent {
+
+}
